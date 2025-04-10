92b5339d92dd5cf9bcbcc14e0a74bb67b2d429aa
Annetta-Agustín <aannetta@frba.utn.edu.ar>
2025-04-09 22:37:33 -0300

    Alumno.java

--- src/main/java/domain/clases/Alumno.java
+++ src/main/java/domain/clases/Alumno.java
@@ -1,1 +1,30 @@
+package domain.clases;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+public class Alumno {
+    @Setter
+    private String nombre;
+
+    @Setter
+    private String legajo;
+    private List<Materia> materiasAprobadas;
+
+    public Alumno() {
+        this.materiasAprobadas = new ArrayList<>();
+    }
+    public boolean estaAprobada(Materia unaMateria) {
+        return this.materiasAprobadas.contains(unaMateria);
+    }
+    public void agregarMateriaAprobada(Materia ... materias) {
+        Collections.addAll(this.materiasAprobadas, materias);
+    }
+    public boolean cumpleCorrelatividad(Materia unaMateria) {
+        return unaMateria.getCorrelativas().stream().allMatch(this::estaAprobada);
+    }
+}
 
